9233c96dcb5281b25a6207dbf386460d8ccac39f
LOCKESST <mlcparobotics@gmail.com>
2017-02-21 11:43:21 -0600

    Created OpModeTemplate

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeTemplate.java
@@ -0,0 +1,118 @@
+/*
+Copyright (c) 2016 Robert Atkinson
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Robert Atkinson nor the names of his contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This file was created by P. Laforet on 2/21/17
+ *
+ * This file is a "bare bones" template that contains the specific setting for our robot. We use a type of
+ * Holonomic drive we call a CrabBot, Where the FrontLeft, FrontRight, BackLeft, and BackRight motors all
+ * use Omniwheels, and there is a CenterMotor, also with an Omniwheel, that runs perpendicular to the other
+ * motors. This allows the robot to be controlled with standard Tank Drive but allows it to strafe side to
+ * side easily.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into the same folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="OpMode Template", group="Linear Opmode")  // @Autonomous(...) is the other common choice
+@Disabled
+public class OpModeTemplate extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+    DcMotor FrontLeft = null;
+    DcMotor FrontRight = null;
+    DcMotor BackLeft = null;
+    DcMotor BackRight = null;
+    DcMotor CenterMotor = null;
+
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        /* eg: Initialize the hardware variables. Note that the strings used here as parameters
+         * to 'get' must correspond to the names assigned during the robot configuration
+         * step (using the FTC Robot Controller app on the phone).
+         */
+
+        FrontLeft = hardwareMap.dcMotor.get("FrontLeft");
+        FrontRight = hardwareMap.dcMotor.get("FrontRight");
+        BackLeft = hardwareMap.dcMotor.get("BackLeft");
+        BackRight = hardwareMap.dcMotor.get("BackRight");
+        CenterMotor = hardwareMap.dcMotor.get("CenterMotor");
+
+
+        // Set the drive motor directions:
+        // "Reverse" the motor that runs backwards when connected directly to the battery
+        FrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+
+
+            // Runs 4 primary wheels in Tank Drive
+            FrontLeft.setPower(gamepad1.left_stick_y);
+            BackLeft.setPower(gamepad1.left_stick_y);
+            FrontRight.setPower(gamepad1.right_stick_y);
+            BackRight.setPower(gamepad1.right_stick_y);
+
+
+            // Controls CenterMotor. When right_trigger is pressed, CrabBot will strafe to the right.
+            // When left_trigger is pressed, CrabBot will strafe to the left.
+            if(gamepad1.right_trigger > 0){
+                CenterMotor.setPower(gamepad1.right_trigger);
+            }
+
+            else{
+                CenterMotor.setPower(-gamepad1.left_trigger);
+            }
+
+        }
+    }
+}
