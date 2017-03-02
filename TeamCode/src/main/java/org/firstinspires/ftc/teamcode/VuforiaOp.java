01f7ed04b0e8e18da049c3fa350411067fb63e5f
mlcparobotics <mlcparobotics@gmail.com>
2017-03-01 18:21:04 -0600

    Laforet is trying to slop his way through Vuforia vision detection. Please bear with him.
    Fully functioning tracking of FTC Vision Targets, up to 4 simultaneously.
    FIXIT3491's Vuforia in FTC Part 4: Using Vuforia with V2.2, stopped at 15:58.
    
    https://www.youtube.com/watch?v=2z-o9Ts8XoE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaOp.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.vuforia.HINT;
 import com.vuforia.Vuforia;
@@ -15,7 +16,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 /**
  * Created by PLAFORET on 2/28/2017.
  */
-
+@Autonomous(name="Vuforia Test", group="Linear Opmode")
 public class VuforiaOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
